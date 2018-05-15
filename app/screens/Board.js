babb64be2e1e5395d278db34ce0ebeccab0133ff
Aaron Ross <ashmortar@gmail.com>
2018-05-14 17:59:26 -0700

    panning now works and pinch zoom is getting closer

--- app/screens/Board.js
+++ app/screens/Board.js
@@ -1,65 +1,174 @@
-import React, { Component } from 'react';
-import { View, Dimensions } from 'react-native';
-import PropTypes from 'prop-types';
-import { autorun } from 'mobx';
-import { TileMap } from 'react-game-kit/native';
+import React, { Component } from "react";
+import { View, Dimensions, PanResponder, Animated } from "react-native";
+import PropTypes from "prop-types";
+import { autorun } from "mobx";
+import { TileMap } from "react-game-kit/native";
 
-import GameStore from '../data/GameStore';
+import GameStore from "../data/GameStore";
+
+function calcDistance(x1, y1, x2, y2) {
+  let dx = Math.abs(x1 - x2);
+  let dy = Math.abs(y1 - y2);
+  return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+}
+
+function calcCenter(x1, y1, x2, y2) {
+  function middle(p1, p2) {
+    return p1 > p2 ? p1 - (p1 - p2) / 2 : p2 - (p2 - p1) / 2;
+  }
+
+  return {
+    x: middle(x1, x2),
+    y: middle(y1, y2)
+  };
+}
+
+function maxOffset(offset, windowDimension, imageDimension) {
+  let max = windowDimension - imageDimension;
+  if (max >= 0) {
+    return 0;
+  }
+  return offset < max ? max : offset;
+}
+
+function calcOffsetByZoom(width, height, imageWidth, imageHeight, zoom) {
+  let xDiff = imageWidth * zoom - width;
+  let yDiff = imageHeight * zoom - height;
+  return {
+    left: -xDiff / 2,
+    top: -yDiff / 2
+  };
+}
 
 export default class Board extends Component {
   static contextTypes = {
-    scale: PropTypes.number,
+    scale: PropTypes.number
   };
   static propTypes = {
-    gameBoard: PropTypes.array,
+    gameBoard: PropTypes.array
   };
 
   constructor(props) {
     super(props);
     this.screenDimensions = Dimensions.get("window");
-    this.tileWidth = 100;
-    this.sourceWidth = 100;
+    this.tileWidth = 50;
+    this.sourceWidth = 50;
     this.gameBoardWidth = this.tileWidth * 40;
     this.scale = 0.2;
     this.blackTilesMap = this.props.gameBoard.map(a => (a.value === 0 ? 1 : 0));
     this.floorTilesMap = this.props.gameBoard.map(a => (a.value > 0 ? 1 : 0));
     this.wallTilesMap = this.props.gameBoard.map(a => (a.value < 0 ? 1 : 0));
     this.state = {
-      stageX: 0,
+      pan: new Animated.ValueXY(),
+      zoom: null,
+      minZoom: null,
+      layoutKnown: false,
+      isZooming: false,
+      isMoving: false,
+      initialDistance: null,
+      initialX: null,
+      initalY: null,
+      offsetTop: 0,
+      offsetLeft: 0,
+      initialTop: 0,
+      initialLeft: 0,
+      initialTopWithoutZoom: 0,
+      initialLeftWithoutZoom: 0,
+      initialZoom: 1,
+      top: 0,
+      left: 0
     };
   }
 
-  componentDidMount() {
-    this.cameraWatcher = autorun(() => {
-      const targetX = Math.round(GameStore.stageX * this.context.scale);
-      this.setState({
-        stageX: targetX,
-      });
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        console.log("on pan responder grant");
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        let touches = evt.nativeEvent.touches;
+        if (touches.length == 2) {
+          let touch1 = touches[0];
+          let touch2 = touches[1];
+
+          this.props.processPinch(
+            touches[0].pageX,
+            touches[0].pageY,
+            touches[1].pageX,
+            touches[1].pageY
+          );
+        } else if (touches.length == 1 && !this.state.isZooming) {
+          this.processTouch(touches[0].pageX, touches[0].pageY);
+        }
+      },
+      onPanResponderRelease: () => {
+        console.log("on pan responder release");
+        this.setState({
+          isZooming: false,
+          isMoving: false
+        });
+        this.props.releaseTouch();
+      }
     });
   }
 
-  componentWillReceiveProps(nextProps, nextContext) {
-    const targetX = Math.round(GameStore.stageX * nextContext.scale);
-    this.setState({
-      stageX: targetX,
-    })
+  processTouch(x, y) {
+    if (!this.state.isMoving) {
+      this.setState({
+        isMoving: true,
+        initialX: x,
+        initialY: y,
+        initialTop: this.state.top,
+        initialLeft: this.state.left
+      });
+    } else {
+      let left = this.state.initialLeft + x - this.state.initialX;
+      let top = this.state.initialTop + y - this.state.initialY;
+
+      this.setState({
+        left:
+          left > 0
+            ? 0
+            : maxOffset(
+                left,
+                this.state.width,
+                this.gameBoardWidth * this.state.zoom
+              ),
+        top:
+          top > 0
+            ? 0
+            : maxOffset(
+                top,
+                this.state.height,
+                this.gameBoardWidth * this.state.zoom
+              )
+      });
+    }
   }
 
   componentWillUnmount() {
     this.cameraWatcher();
-  }
-
-  getWrapperStyles() {
-    return {
-      position: 'absolute',
-      transform: [{ translateX: this.state.stageX }],
-    };
+    this.state.pan.x.removeAllListeners();
+    this.state.pan.y.removeAllListeners();
   }
 
   render() {
-    console.log(`board render began and scale=${this.context.scale}`);
+    console.log("opacitiy?");
     return (
-      <View >
+      <View
+        style={{
+          position: "absolute",
+          top: this.state.offsetTop + this.state.top,
+          left: this.state.offsetLeft + this.state.left,
+          width: this.gameBoardWidth * this.state.zoom,
+          height: this.gameBoardWidth * this.state.zoom
+        }}
+        {...this._panResponder.panHandlers}
+      >
         <TileMap
           src={require("../data/images/Black_square.jpeg")}
           tileSize={this.tileWidth}
@@ -68,7 +177,6 @@ export default class Board extends Component {
           sourceWidth={this.sourceWidth}
           layers={[this.blackTilesMap]}
           scale={this.context.scale}
-
         />
         <TileMap
           src={require("../data/images/Magenta-square_100px.gif")}
@@ -78,7 +186,6 @@ export default class Board extends Component {
           sourceWidth={this.sourceWidth}
           layers={[this.wallTilesMap]}
           scale={this.context.scale}
-
         />
         <TileMap
           src={require("../data/images/Cyan-square.png")}
--- app/screens/Engine.js
+++ app/screens/Engine.js
@@ -1,76 +1,191 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Dimensions, TouchableOpacity, View } from "react-native";
+import { Dimensions } from "react-native";
 import { Loop, Stage } from "react-game-kit/native";
 
-import Board from './Board';
+import Board from "./Board";
+
+function calcDistance(x1, y1, x2, y2) {
+  let dx = Math.abs(x1 - x2);
+  let dy = Math.abs(y1 - y2);
+  return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+}
+
+function calcCenter(x1, y1, x2, y2) {
+  function middle(p1, p2) {
+    return p1 > p2 ? p1 - (p1 - p2) / 2 : p2 - (p2 - p1) / 2;
+  }
+
+  return {
+    x: middle(x1, x2),
+    y: middle(y1, y2)
+  };
+}
+
+function maxOffset(offset, windowDimension, imageDimension) {
+  let max = windowDimension - imageDimension;
+  if (max >= 0) {
+    return 0;
+  }
+  return offset < max ? max : offset;
+}
+
+function calcOffsetByZoom(width, height, imageWidth, imageHeight, zoom) {
+  let xDiff = imageWidth * zoom - width;
+  let yDiff = imageHeight * zoom - height;
+  return {
+    left: -xDiff / 2,
+    top: -yDiff / 2
+  };
+}
 
 export default class Engine extends Component {
   static propTypes = {
     gameBoard: PropTypes.array,
-    children: PropTypes.any,
     tilesInRow: PropTypes.number,
     boardFinished: PropTypes.bool
   };
+
   constructor(props) {
     super(props);
     console.log("Engine");
+    this._onLayout = this._onLayout.bind(this);
     this.screenDimensions = Dimensions.get("window");
     this.tileWidth = 100;
     this.sourceWidth = 100;
     this.gameBoardWidth = this.tileWidth * props.tilesInRow;
-    this.scale = 0.2;
     this.state = {
       stageHeight: this.screenDimensions.height,
       stageWidth: this.screenDimensions.width,
+      zoom: 1,
+      minZoom: null,
+      layoutKnown: false,
+      isZooming: false,
+      isMoving: false,
+      initialDistance: null,
+      initialX: null,
+      initalY: null,
+      offsetTop: 0,
+      offsetLeft: 0,
+      initialTop: 0,
+      initialLeft: 0,
+      initialTopWithoutZoom: 0,
+      initialLeftWithoutZoom: 0,
+      initialZoom: 1,
+      top: 0,
+      left: 0
     };
   }
 
-  zoomIn = () => {
-    console.log('zoomIn');
+  releaseTouch = () => {
     this.setState({
-      stageHeight: this.state.stageHeight / 2,
-      stageWidth: this.state.stageWidth / 2,
-    });
+      isZooming: false,
+      isMoving: false
+  });
   }
 
-  zoomOut = () => {
-    console.log('zoomOut');
+  processPinch = (x1, y1, x2, y2) => {
+    console.log('process pinch');
+    let distance = calcDistance(x1, y1, x2, y2);
+    let center = calcCenter(x1, y1, x2, y2);
+
+    if (!this.state.isZooming) {
+      let offsetByZoom = calcOffsetByZoom(
+        this.state.width,
+        this.state.height,
+        this.props.imageWidth,
+        this.props.imageHeight,
+        this.state.zoom
+      );
+      this.setState({
+        isZooming: true,
+        initialDistance: distance,
+        initialX: center.x,
+        initialY: center.y,
+        initialTop: this.state.top,
+        initialLeft: this.state.left,
+        initialZoom: this.state.zoom,
+        initialTopWithoutZoom: this.state.top - offsetByZoom.top,
+        initialLeftWithoutZoom: this.state.left - offsetByZoom.left
+      });
+    } else {
+      let touchZoom = distance / this.state.initialDistance;
+      let zoom =
+        touchZoom * this.state.initialZoom > this.state.minZoom
+          ? touchZoom * this.state.initialZoom
+          : this.state.minZoom;
+
+      let offsetByZoom = calcOffsetByZoom(
+        this.state.width,
+        this.state.height,
+        this.props.imageWidth,
+        this.props.imageHeight,
+        zoom
+      );
+      let left =
+        this.state.initialLeftWithoutZoom * touchZoom + offsetByZoom.left;
+      let top = this.state.initialTopWithoutZoom * touchZoom + offsetByZoom.top;
+      console.log('zoom', zoom)
+      this.setState({
+        zoom: zoom,
+        left: 0,
+        top: 0,
+        left:
+          left > 0
+            ? 0
+            : maxOffset(left, this.state.width, this.props.imageWidth * zoom),
+        top:
+          top > 0
+            ? 0
+            : maxOffset(top, this.state.height, this.props.imageHeight * zoom)
+      });
+    }
+  }
+
+  _onLayout(event) {
+    let layout = event.nativeEvent.layout;
+
+    if (
+      layout.width === this.state.width &&
+      layout.height === this.state.height
+    ) {
+      return;
+    }
+
+    let zoom = layout.width / this.props.imageWidth;
+
+    let offsetTop =
+      layout.height > this.props.imageHeight * zoom
+        ? (layout.height - this.props.imageHeight * zoom) / 2
+        : 0;
+
     this.setState({
-      stageHeight: this.state.stageHeight * 2,
-      stageWidth: this.state.stageWidth * 2,
+      layoutKnown: true,
+      width: layout.width,
+      height: layout.height,
+      zoom: zoom,
+      offsetTop: offsetTop,
+      minZoom: zoom
     });
   }
 
   render() {
-    console.log('engine render began');
+    const { left, top, zoom } = this.state;
+    console.log("engine render began");
     return (
       <Loop style={{ backgroundColor: "#212121" }}>
         <Stage
-          width={this.state.stageWidth}
-          height={this.state.stageHeight}
-          style={{
-            backgroundColor: "#515151"
-            // width: Math.floor(this.screenDimensions.width * 0.98),
-            // height: Math.floor(this.screenDimensions.height * 0.8),
-            // marginLeft: Math.floor(this.screenDimensions.width * 0.01),
-            // marginRight: Math.floor(this.screenDimensions.width * 0.01),
-            // marginTop: Math.floor(this.screenDimensions.width * 0.01)
-          }}
+          width={this.state.stageWidth / this.state.zoom}
+          height={this.state.stageHeight / this.state.zoom}
+          style={{ backgroundColor: "#515151" }}
         >
-          {this.props.boardFinished ?
-            <Board gameBoard={this.props.gameBoard} />
-          : null}
-          <View style={{ flexDirection: 'column' }}>
-            <TouchableOpacity
-              style={{ backgroundColor: '#fff', width: 50, height: 50 }}
-              onPress={this.zoomIn}
+          {this.props.boardFinished ? (
+            <Board
+              gameBoard={this.props.gameBoard}
+              processPinch={this.processPinch}
+              releaseTouch={this.releaseTouch}
             />
-            <TouchableOpacity
-              style={{ backgroundColor: '#ccc', width: 50, height: 50 }}
-              onPress={this.zoomOut}
-            />
-          </View>
+          ) : null}
         </Stage>
       </Loop>
     );
